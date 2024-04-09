c1eaf30766d4a21fddc9524095eb6bb2ec682b7c
Rikudo10 <giorgisamxaradze16@gmail.com>
2024-04-10 01:25:43 +0400

    header footer finish

--- MultiDev/src/components/Header.vue
+++ MultiDev/src/components/Header.vue
@@ -24,16 +24,16 @@ onMounted(() => {
       </router-link>
       <div class="w-full md:block md:w-auto" id="navbar-dropdown">
         <ul
-          class="flex flex-col gap-20 font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          class="flex flex-col md:gap-20 gap-4 font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
-          <router-link to="/" class="py-2 text-2xl px-3 rounded md:bg-transparent md:p-0 text-black" active-class="active-link">
+          <router-link to="/" class="py-2 md:text-2xl text-xl px-3 rounded md:bg-transparent md:p-0 text-black" active-class="active-link">
             <li class="pt-3">Home</li>
           </router-link>
           <li class="pt-3">
             <button
             :class="{ 'color-red': isDropdownActive }"
               @click="toggleDropdown"
-              class="flex  pt-3 items-center justify-between text-2xl w-full py-2 px-3 text-black rounded md:border-0 md:p-0 md:w-auto"
+              class="flex  pt-3 items-center justify-between md:text-2xl text-xl w-full py-2 px-3 text-black rounded md:border-0 md:p-0 md:w-auto"
             >
               Services
               <svg
@@ -57,7 +57,7 @@ onMounted(() => {
             <div
               v-show="isDropdownActive"
               id="dropdownNavbar"
-              class="z-10  font-normal absolute -translate-x-[27%] translate-y-16 w-[35%] border-t-2 bg-[white] divide-y  divide-[#D8D8D8]  shadow px-10 py-10 border-x border-b border-[#D8D8D8]"
+              class="z-10  font-normal absolute -translate-x-[27%] translate-y-16 w-full md:w-[35%] border-t-2 bg-[white] divide-y  divide-[#D8D8D8]  shadow px-10 py-10 border-x border-b border-[#D8D8D8]"
             >
               <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
                 <img class="pb-10" src="../assets/icons/services.svg" alt="">
@@ -67,13 +67,13 @@ onMounted(() => {
               </div>
             </div>
           </li>
-          <router-link to="/about" class="py-2 text-2xl px-3 rounded md:bg-transparent md:p-0 text-black" active-class="active-link">
+          <router-link to="/about" class="py-2 md:text-2xl text-xl px-3 rounded md:bg-transparent md:p-0 text-black" active-class="active-link">
             <li class="pt-3">About Us</li>
           </router-link>
           <router-link to="/contact">
             <li>
               <button
-                class="bg-[#F23A3A] text-white py-[10px] px-[30px] text-2xl rounded-full"
+                class="bg-[#F23A3A] text-white md:py-[10px] md:px-[30px] md:text-2xl py-3 px-3 text-base rounded-full"
               >
                 Contact Us
               </button>
