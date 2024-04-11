10aa4398f98cb3d3b92ce11e8d0ab3446d927666
Rikudo10 <giorgisamxaradze16@gmail.com>
2024-04-12 03:15:51 +0400

    accordion

--- MultiDev/src/components/HeaderComp/Accordion.vue
+++ MultiDev/src/components/HeaderComp/Accordion.vue
@@ -42,7 +42,7 @@ onMounted(() => {
 
 <div id="accordion-open" data-accordion="open">
   <h2 id="accordion-collapse-heading-1">
-    <button type="button" class="flex items-center black mybg w-full p-5 pl-0 pb-2 font-medium rtl:text-right gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
+    <button type="button" class="flex items-center black mybg w-full p-5  pl-0 pb-2 font-medium rtl:text-right gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
   <span>Front-End Development</span>
   <svg  class="w-3 h-3 rotate-90 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
@@ -50,7 +50,7 @@ onMounted(() => {
 </button>
   </h2>
   <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
-    <div class=" pl-0 pb-2 pt-2 ml-5">
+    <div class=" pl-0  pb-2 pt-2 ml-2">
       <p class="mb-2 text-xs flex gap-2 font-medium	"><img src="../../assets/icons/react.svg" alt="">{{ HeaderInfo[0].FrontEnd[0].title }}</p>
       <p class="mb-2 text-xs flex gap-2 font-medium	"><img src="../../assets/icons/angular.svg" alt="">{{ HeaderInfo[0].FrontEnd[0].title1 }}</p>
       <p class="mb-2 text-xs flex gap-2 font-medium	"><img src="../../assets/icons/vue.svg" alt="">{{ HeaderInfo[0].FrontEnd[0].title2 }}</p>
@@ -66,7 +66,7 @@ onMounted(() => {
     </button>
   </h2>
   <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
-    <div class="p-5 pb-2 pt-2 ml-5">
+    <div class=" pl-0 pb-2 pt-2 ml-2">
       <p class="mb-2 text-xs flex gap-2 font-medium	"><img src="../../assets/icons/spring.svg" alt="">{{ HeaderInfo[0].BackEnd[0].title }}</p>
       <p class="mb-2 text-xs flex gap-2 font-medium	"><img src="../../assets/icons/laravel.svg" alt="">{{ HeaderInfo[0].BackEnd[0].title1 }}</p>
       <p class="mb-2 text-xs flex gap-2 font-medium	"><img src="../../assets/icons/django.svg" alt="">{{ HeaderInfo[0].BackEnd[0].title2 }}</p>
@@ -83,7 +83,7 @@ onMounted(() => {
     </button>
   </h2>
   <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
-    <div class="p-5 pt-2 pb-2">
+    <div class="pl-0 pb-2 pt-2 ml-2">
       
     </div>
   </div>
@@ -97,7 +97,7 @@ onMounted(() => {
     </button>
   </h2>
   <div id="accordion-collapse-body-4" class="hidden" aria-labelledby="accordion-collapse-heading-3">
-    <div class="p-5 pt-2 pb-2">
+    <div class="pl-0 pb-2 pt-2 ml-2">
       
     </div>
   </div>
@@ -111,7 +111,7 @@ onMounted(() => {
     </button>
   </h2>
   <div id="accordion-collapse-body-5" class="hidden" aria-labelledby="accordion-collapse-heading-3">
-    <div class="p-5 pt-2 pb-2">
+    <div class="pl-0 pb-2 pt-2 ml-2">
       
     </div>
   </div>
@@ -124,7 +124,7 @@ onMounted(() => {
     </button>
   </h2>
   <div id="accordion-collapse-body-6" class="hidden" aria-labelledby="accordion-collapse-heading-3">
-    <div class="p-5 pt-2 pb-2">
+    <div class="pl-0 pb-2 pt-2 ml-2">
       
     </div>
   </div>
